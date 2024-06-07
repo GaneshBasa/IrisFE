953a80970dd4f344149dc68ac25bfc7ab459c30d
Ganesh Basa <basaganesh1@gmail.com>
2024-06-07 22:06:39 +0530

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {
+    fetch( 'https://iris-be.vercel.app' )
+    .then( res => {
+      if ( res.ok ) return res.json()
+    } )
+    .then( console.log )
+    .catch( console.error )
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
