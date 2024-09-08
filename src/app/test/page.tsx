707329fc6d17cd9507b05a423d66d75a86f28557
Ganesh Basa <basaganesh1@gmail.com>
2024-09-08 13:30:12 +0530

    Fixed Test Page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,7 +2,7 @@
 
 import { FC, useEffect, useState } from 'react'
 import axios from 'axios'
-import { FlaskConical } from 'lucide-react'
+import { LuFlaskConical } from 'react-icons/lu'
 
 import { be, randomUserAPI } from '@common/config'
 
@@ -22,7 +22,7 @@ const TestPage : FC = () => {
   return (
     <section className='min-h-screen flex'>
       <div className='m-auto flex portrait:flex-col landscape:space-x-2.5 portrait:space-y-2.5 text-center'>
-        <FlaskConical className='size-12 m-auto' />
+        <LuFlaskConical className='size-12 m-auto' />
         <h1 className='text-5xl font-extrabold'>
           Test Page
         </h1>
