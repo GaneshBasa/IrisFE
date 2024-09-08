869c4955494c8822c4633dbde02b87f8dc2a374a
Ganesh Basa <basaganesh1@gmail.com>
2024-09-08 13:29:18 +0530

    Fix Index Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
-import { Construction } from 'lucide-react'
+import { LuConstruction } from 'react-icons/lu'
 
 
 const RootPage : FC = () => (
   <section className='min-h-screen flex'>
-    <div className='m-auto flex portrait:flex-col landscape:space-x-2.5 portrait:space-y-2.5 text-center'>
-      <Construction className='size-12 m-auto' />
+    <div className='m-auto flex portrait:flex-col landscape:space-x-2.5 portrait:space-y-2.5 text-center text-primary'>
+      <LuConstruction className='size-12 m-auto' />
       <h1 className='text-5xl font-extrabold'>
         Site Under Construction !
       </h1>
